a7ce786b20fa76e49ffc0ec0be6e2e841c4a39a8
Hunter Goller <65684802+HunterG6700@users.noreply.github.com>
2022-05-26 12:08:10 -0400

    test

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: '/resume/',
   transpileDependencies: true,
 
   pluginOptions: {
-    vuetify: {}
+    vuetify: {
+			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+		}
   }
 })
