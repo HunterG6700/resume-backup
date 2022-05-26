b47cab8a0be91708dceb80aa06605c0a67d33aae
Hunter Goller <65684802+HunterG6700@users.noreply.github.com>
2022-05-26 12:40:58 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,14 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: '/resume/',
   transpileDependencies: true,
+	pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html'
+    }
+  },
+  
 
   pluginOptions: {
     vuetify: {
